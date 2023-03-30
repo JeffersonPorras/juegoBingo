0ee39b00dfe084606550ee2ba1bf9e122d0eb960
Jefferson Porras <jeffersonporras.jp@gmail.com>
2023-03-29 22:15:39 -0500

    ubicando numero entre rango de letras y mostrando el resultado de numero mas su letra correspondiente

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const numeroElegidoBalotera = document.getElementById('numeroElegido')
+const letraCorrespondienteAlNumero = document.getElementById('resultadoDeLetraYNumero')
 let numeroAleatoriosAlmacenados = []
 
 
@@ -14,11 +15,30 @@ function mostrarNumeroAleatorio() {
     console.log(numeroAleatorio);
     numeroAleatoriosAlmacenados.push(numeroAleatorio)
     mostrarNumeroElegido(numeroAleatorio)
+    MostrarLetraCorrespondienteANumero(numeroAleatorio)
 }
 
 function mostrarNumeroElegido(resultado){
     numeroElegidoBalotera.innerHTML = resultado
   }
+  function mostrarLetraConNumero(resultado){
+    letraCorrespondienteAlNumero.innerHTML = resultado
+  }
 function generarNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
+function MostrarLetraCorrespondienteANumero(numero) {
+  if (numero <= 20) {
+    mostrarLetraConNumero(`B ${numero}`)
+  }else if(numero > 21 && numero <= 40){
+    mostrarLetraConNumero(`I ${numero}`)
+  }else if(numero > 41 && numero <= 60){
+    mostrarLetraConNumero(`N ${numero}`)
+  }else if(numero > 61 && numero <= 80){
+    mostrarLetraConNumero(`G ${numero}`)
+  }else{
+    mostrarLetraConNumero(`O ${numero}`)
+  }
+
+}
